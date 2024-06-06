4b54f114725ff2b0488d1c8529cd8d6c1d509125
Kiru-sivakumar <157816684+Kiru-sivakumar@users.noreply.github.com>
2024-06-06 08:07:40 -1200

    Update exercise_2.py

--- exercises/exercise_2.py
+++ exercises/exercise_2.py
@@ -1,1 +1,6 @@
-# Your solution to Exercise 2
+m = int(input('enter the m number:'))
+n = int(input("enter the n number:"))
+#print n number m times in a row
+for i in range(m):
+    print(n,end = ' ')
+  
