099b25169f42221f84d183f83a3ec49e05cd16ef
Kiru-sivakumar <157816684+Kiru-sivakumar@users.noreply.github.com>
2024-06-06 08:45:04 -1200

    Update exercise_9.py

--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,1 +1,6 @@
-# Your solution to Exercise 9
+a= int(input ('enter the first num:'))
+b = int(input('enter the second num:'))
+c = int(input('enter the third num:'))
+for i in range (a,b+1):
+    if i%c == 0:
+        print(i, end = ' ')
